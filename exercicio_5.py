34646f56ce62e2cdf9dcf24aafe9813b2e5425d6
xZ0842 <enzocaveira@hotmail.com>
2026-05-06 13:52:03 -0300

    ex_5.py

--- exercicio_5.py
+++ exercicio_5.py
@@ -18,7 +18,7 @@ No exemplo acima, você imprimiria:
 
 "O organismo com maior média é o da posição 1 da lista."
 
-Porque o organismo da posição 0 tem média de (100 + 200 + 300) / 3 = 200, o organismo da posição 0
+Porque o organismo da posição 0 tem média de (100 + 200 + 300) / 3 = 200, o organismo da posição 1
 tem média de (1 + 99 + 10000) / 3 = 3366,66 e o da posição 2 tem média de (1 + 1 + 1) / 3 = 1.
 
 Logo o da posição 1 é maior.
@@ -34,9 +34,32 @@ O organismo com maior média é o da posição 1 da lista.
 Dica: Utilize mais de um for para resolver o exercício, um para a lista de organismos e um para cada lista. Cuidado com a identação.
 
 O cálculo de média já foi feito em sala e pode ser usado de exemplo.
-"""
+
 
 # Lista
 lista_de_organismos = [[50, 50, 50], [125, 99, 12], [19, 91, 42], [40, 189, 0], [1, 0, 0], [100, 100, 70], [99, 12, 12]]
 
 # Fazer a partir daqui
+"""
+
+lista_de_organismos = [[50, 50, 50], [125, 99, 12], [19, 91, 42], [40, 189, 0], [1, 0, 0], [100, 100, 70], [99, 12, 12]]
+
+maior_media = 0
+posicao = 0
+
+for i in range(len(lista_de_organismos)):
+
+    organismo = lista_de_organismos[i]
+
+    soma = 0
+
+    for leitura in organismo:
+        soma = soma + leitura
+
+    media = soma / len(organismo)
+
+    if media > maior_media:
+        maior_media = media
+        posicao = i
+
+print(f"O organismo com maior média é o da posição {posicao} da lista.")
