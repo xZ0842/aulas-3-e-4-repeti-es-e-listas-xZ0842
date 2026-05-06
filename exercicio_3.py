dcd09877c7f8fecc538d06937089736b8f0d69b1
xZ0842 <enzocaveira@hotmail.com>
2026-05-06 12:08:37 -0300

    ex_3
    Update exercise label and implement filtering for odd numbers.

--- exercicio_3.py
+++ exercicio_3.py
@@ -1,5 +1,5 @@
 """
-#### Exercício 2 - Filtrando uma lista.
+#### Exercício 3 - Filtrando uma lista.
 
 Receba uma lista de input do usuário.
 
@@ -17,9 +17,24 @@ Exemplos:
 Digite a sua lista (separando os números por vírgula): 1, 2, 3, 4, 5
 Resposta:
 Os números ímpares são [1, 3, 5]
-"""
+
 
 # Código para pegar a lista
 lista = [*map(int, input("Digite a sua lista (separando os números por vírgula): ").split(","))]
 
 # Fazer a partir daqui...
+"""
+
+
+lista = [*map(int, input("Digite a sua lista (separando os números por vírgula): ").split(","))]
+
+impares = []
+
+for numero in lista:
+    if numero % 2 != 0:
+        print("Ímpar")
+        impares.append(numero)
+    else:
+        print("Par")
+
+print(f"Os números ímpares são {impares}")
